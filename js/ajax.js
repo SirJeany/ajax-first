30dd937ea246713d4a7f026faf30e97b180177df
SirJeany <jmvanriet1@gmail.com>
2020-03-23 17:09:22 +0200

    Introduced Promises

--- js/ajax.js
+++ js/ajax.js
@@ -1,3 +1,27 @@
 'use strict';
 
 let approval = 'Not Approved';
+
+const result = document.getElementById('result');
+
+function getApproval() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            // resolve('Approved');
+            reject('Nope');
+        }, 2000);
+    })
+}
+
+getApproval().then(
+    (resolvedApproval) => {
+    result.textContent = resolvedApproval;
+});
+
+getApproval().catch(
+    (rejected) => {
+        result.textContent = rejected;
+    }
+);
+
+result.textContent = approval;
